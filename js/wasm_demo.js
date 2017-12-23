23bc15eb81e4eff3d349f56d18fc07286bd28b42
Adam M Ludvik <adam.m.ludvik@gmail.com>
2017-12-22 23:28:00 -0600

    Extend example

--- js/wasm_demo.js
+++ js/wasm_demo.js
@@ -1,7 +1,17 @@
 console.log("rust wasm demo");
+
+const imports = {
+  env: {
+    console_log: function(num) {
+      console.log(num);
+    }
+  }
+};
+
 fetch('wasm_demo.wasm.min')
-.then(response => response.arrayBuffer())
-.then(bytes => WebAssembly.instantiate(bytes, {}))
-.then(results => {
-    console.log(results.instance.exports.add_one(41));
-});
+  .then(response => response.arrayBuffer())
+  .then(bytes => WebAssembly.instantiate(bytes, imports))
+  .then(results => {
+      console.log(results.instance.exports.times_two(41));
+      results.instance.exports.print_num();
+  });
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,15 @@
+extern "C" {
+    fn console_log(_: i32);
+}
+
 #[no_mangle]
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+pub fn print_num() {
+    unsafe {
+        console_log(19);
+    }
+}
+
+#[no_mangle]
+pub fn times_two(x: i32) -> i32 {
+    x * 2
 }
